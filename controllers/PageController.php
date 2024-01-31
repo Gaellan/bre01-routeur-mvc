6b5218de3ed8089421ad08dff52b0b514e81c395
Gaellan <doucet.mari@gmail.com>
2024-01-31 13:46:41 +0100

    step 3

---
+++ controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author : Gaellan
+ * @link : https://github.com/Gaellan
+ */
+
+class PageController
+{
+    public function __construct()
+    {
+
+    }
+
+    public function home() : void
+    {
+        $route = "home";
+
+        require "templates/layout.phtml";
+    }
+
+    public function about() : void
+    {
+        $route = "about";
+
+        require "templates/layout.phtml";
+    }
+
+    public function error404() : void
+    {
+        $route = "404";
+
+        require "templates/layout.phtml";
+    }
+}
