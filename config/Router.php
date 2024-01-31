8aefff1f96ef0db3cc0655f649a8d0c017765232
Gaellan <doucet.mari@gmail.com>
2024-01-31 13:43:09 +0100

    step 2

--- config/Router.php
+++ config/Router.php
@@ -6,5 +6,24 @@
 
 class Router
 {
+    public function __construct()
+    {
 
+    }
+
+    public function handleRequest(array $get) : void
+    {
+       if(isset($get["route"]) && $get["route"] === "a-propos")
+       {
+
+       }
+       else if(!isset($get["route"]))
+       {
+
+       }
+       else
+       {
+
+       }
+    }
 }
